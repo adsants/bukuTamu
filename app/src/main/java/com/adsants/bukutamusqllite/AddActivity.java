5e1bea1b296b0a3737a50688d2634ecba4f80838
adsants <mail.adisantoso@gmail.com>
2018-08-20 08:47:17 +0700

    Pertama

---
+++ app/src/main/java/com/adsants/bukutamusqllite/AddActivity.java
@@ -0,0 +1,47 @@
+package com.adsants.bukutamusqllite;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.Toast;
+
+public class AddActivity extends AppCompatActivity {
+
+    RadioGroup radio_kelamin;
+    RadioButton input_radio_cewek,input_radio_cowok;
+    EditText input_nama,input_alamat;
+    Button btn_simpan;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add);
+
+        radio_kelamin       = findViewById(R.id.radio_kelamin);
+        input_radio_cowok   = findViewById(R.id.input_radio_cowok);
+        input_radio_cewek   = findViewById(R.id.input_radio_cewek);
+        input_nama          = findViewById(R.id.input_nama);
+        input_alamat        = findViewById(R.id.input_alamat);
+        btn_simpan        = findViewById(R.id.btn_simpan);
+
+        btn_simpan.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext(),  input_nama.getText().toString(), Toast.LENGTH_LONG).show();
+            }
+        });
+
+        getSupportActionBar().setTitle("Tambah Data");
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
+    @Override
+    public boolean onSupportNavigateUp(){
+        finish();
+        return true;
+    }
+}
