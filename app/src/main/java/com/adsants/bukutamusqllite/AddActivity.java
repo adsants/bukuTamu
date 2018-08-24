8cd4eaf25ae5dcdf001ea8dcb8b286ca5016d4b4
adsants <mail.adisantoso@gmail.com>
2018-08-24 22:05:31 +0700

    Full sudah kelar SQlite

--- app/src/main/java/com/adsants/bukutamusqllite/AddActivity.java
+++ app/src/main/java/com/adsants/bukutamusqllite/AddActivity.java
@@ -1,5 +1,6 @@
 package com.adsants.bukutamusqllite;
 
+import android.database.sqlite.SQLiteDatabase;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -10,17 +11,29 @@ import android.widget.RadioButton;
 import android.widget.RadioGroup;
 import android.widget.Toast;
 
+import com.adsants.bukutamusqllite.helper.SqliteHelper;
+import com.andexert.library.RippleView;
+
 public class AddActivity extends AppCompatActivity {
 
     RadioGroup radio_kelamin;
     RadioButton input_radio_cewek,input_radio_cowok;
     EditText input_nama,input_alamat;
     Button btn_simpan;
+    RippleView ripple_simpan;
+
+    String jenisKelamin;
+
+    SqliteHelper sqliteHelper;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add);
+
+        jenisKelamin = "";
+
+        sqliteHelper = new SqliteHelper(this);
 
         radio_kelamin       = findViewById(R.id.radio_kelamin);
         input_radio_cowok   = findViewById(R.id.input_radio_cowok);
@@ -28,15 +41,49 @@ public class AddActivity extends AppCompatActivity {
         input_nama          = findViewById(R.id.input_nama);
         input_alamat        = findViewById(R.id.input_alamat);
         btn_simpan        = findViewById(R.id.btn_simpan);
+        ripple_simpan        = findViewById(R.id.ripple_simpan);
+
+        radio_kelamin.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup radioGroup, int i) {
+                switch(i){
+                    case R.id.input_radio_cewek: jenisKelamin = "Perempuan";
+                        break;
+                    case R.id.input_radio_cowok: jenisKelamin = "Laki-Laki";
+                        break;
+                }
+            }
+        });
 
         btn_simpan.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-               // Toast.makeText(getApplicationContext(),  "Nama : "+ input_nama.getText().toString() +", Alamatku adalah  : "+ input_alamat.getText().toString() , Toast.LENGTH_LONG).show();
-                Snackbar.make(findViewById(R.id.til), "Contoh sebuah Snackbar", Snackbar.LENGTH_LONG).setAction("Action!", this).show();
+               /// diganti di Ripple onclick
             }
         });
 
+        ripple_simpan.setOnRippleCompleteListener(new RippleView.OnRippleCompleteListener() {
+            @Override
+            public void onComplete(RippleView rippleView) {
+
+                if(input_nama.getText().toString().equals("")){
+                    Toast.makeText(getApplicationContext(),  "Masukkan Nama terlebih dahulu." , Toast.LENGTH_LONG).show();
+                    input_nama.requestFocus();
+                }
+                else if(jenisKelamin.equals("")){
+                    Toast.makeText(getApplicationContext(),  "Pilih Jenis Kelamin." , Toast.LENGTH_LONG).show();
+                }
+                else if(input_alamat.getText().toString().equals("")){
+                    Toast.makeText(getApplicationContext(),  "Masukkan Alamat terlebih dahulu.." , Toast.LENGTH_LONG).show();
+                    input_alamat.requestFocus();
+                }
+                else {
+                    simpan_data();
+                    //Toast.makeText(getApplicationContext(), "Nama : " + input_nama.getText().toString() + ", Alamatku adalah  : " + input_alamat.getText().toString(), Toast.LENGTH_LONG).show();
+
+                }
+            }
+        });
 
         getSupportActionBar().setTitle("Tambah Data");
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
@@ -46,5 +93,16 @@ public class AddActivity extends AppCompatActivity {
     public boolean onSupportNavigateUp(){
         finish();
         return true;
+    }
+
+    private void simpan_data(){
+
+        SQLiteDatabase database = sqliteHelper.getWritableDatabase();
+        database.execSQL("insert into transaksi (nama,jenis_kelamin,alamat) values ('"+input_nama.getText().toString()+"','"+jenisKelamin+"', '"+input_alamat.getText().toString()+"')");
+
+        Toast.makeText(getApplicationContext(), "Data berhasil disimpan.", Toast.LENGTH_LONG).show();
+
+        finish();
+
     }
 }
--- app/src/main/java/com/adsants/bukutamusqllite/MainActivity.java
+++ app/src/main/java/com/adsants/bukutamusqllite/MainActivity.java
@@ -1,25 +1,65 @@
 package com.adsants.bukutamusqllite;
 
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.WindowManager;
+import android.widget.AdapterView;
+import android.widget.ListView;
+import android.widget.SimpleAdapter;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.adsants.bukutamusqllite.helper.SqliteHelper;
+
+import java.text.NumberFormat;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
 
+    ListView list_bukutamu;
+    TextView jumlah_laki, jumlah_perempuan, jumlah_keseluruhan;
+    SwipeRefreshLayout swipe_refresh;
+
+    ArrayList<HashMap<String, String>>  data_buku_tamu;
+
+    public static String transaksi_id,tgl_mulai,tgl_akhir;
+    public static TextView text_hasil_pencarian;
+    public static boolean query_pencarian;
+
+    String query_data,query_total;
+
+    SqliteHelper sqliteHelper;
+    Cursor cursor;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-
-
-
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+
+//        Belajar Layout
+
+        // log.d adalah untuk debug
+        Log.d("contoh","hasilnya satu");
+
+        // log.e adalah untuk error
+        Log.e("contoh2","hasilnya Dua");
 
         toolbar.setTitle("Buku Tamu by adsants");
         setSupportActionBar(toolbar);
@@ -37,6 +77,177 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(new Intent(MainActivity.this, AddActivity.class));
             }
         });
+
+        list_bukutamu       = findViewById(R.id.list_view);
+        data_buku_tamu      = new ArrayList<>();
+
+        sqliteHelper        = new SqliteHelper(this);
+
+        jumlah_perempuan    = findViewById(R.id.jumlah_perempuan);
+        jumlah_laki         = findViewById(R.id.jumlah_laki);
+        jumlah_keseluruhan  = findViewById(R.id.jumlah_keseluruhan);
+        text_hasil_pencarian  = findViewById(R.id.text_hasil_pencarian);
+
+        swipe_refresh       = findViewById(R.id.swipe_refresh);
+
+        swipe_refresh.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+            @Override
+            public void onRefresh() {
+
+                query_data  =   "select *,strftime('%d-%m-%Y', tanggal_input) AS tgl from transaksi order by tanggal_input desc";
+                query_total =   "select count(*) as jumlah_total,(select count(*) from transaksi where jenis_kelamin='Laki-Laki') as jumlah_laki," +
+                        "(select count(*) from transaksi where jenis_kelamin='Perempuan') as jumlah_perempuan " +
+                        "from transaksi ";
+
+                kasAdapter();
+            }
+        });
+
+    }
+
+    @Override
+    public void onResume(){
+        super.onResume();
+
+        query_data  =   "select *,strftime('%d-%m-%Y', tanggal_input) AS tgl from transaksi order by tanggal_input desc";
+        query_total =   "select count(*) as jumlah_total," +
+                "(select count(*) from transaksi where jenis_kelamin='Laki-Laki') as jumlah_laki," +
+                "(select count(*) from transaksi where jenis_kelamin='Perempuan') as jumlah_perempuan " +
+                "from transaksi ";
+
+        if(query_pencarian){
+            query_data  =   "select *,strftime('%d-%m-%Y', tanggal_input) AS tgl from transaksi" +
+                    " where (tanggal_input >= '"+tgl_mulai+"') and (tanggal_input <= '"+tgl_akhir+"')" +
+                    "order by tanggal_input desc";
+            query_total =   "select count(*) as jumlah_total," +
+                    "(select count(*) from transaksi where jenis_kelamin='Laki-Laki' and  (tanggal_input >= '"+tgl_mulai+"') and (tanggal_input <= '"+tgl_akhir+"')) as jumlah_laki," +
+                    "(select count(*) from transaksi where jenis_kelamin='Perempuan' and  (tanggal_input >= '"+tgl_mulai+"') and (tanggal_input <= '"+tgl_akhir+"')) as jumlah_perempuan " +
+                    "from transaksi where (tanggal_input >= '"+tgl_mulai+"') and (tanggal_input <= '"+tgl_akhir+"')";
+        }
+
+        kasAdapter();
+    }
+
+
+
+    protected void kasAdapter() {
+
+        data_buku_tamu.clear();
+        list_bukutamu.setAdapter(null);
+
+        SQLiteDatabase db = sqliteHelper.getReadableDatabase();
+        cursor = db.rawQuery(query_data, null);
+        cursor.moveToFirst();
+
+        for(int i=0; i < cursor.getCount(); i++ ){
+            cursor.moveToPosition(i);
+
+            HashMap<String, String > map = new HashMap<>();
+
+            map.put("transaksi_id", cursor.getString(0));
+            map.put("nama", cursor.getString(2));
+            map.put("jkl", cursor.getString(1));
+            map.put("alamat", cursor.getString(3));
+            map.put("tanggal", cursor.getString(5));
+
+            data_buku_tamu.add(map);
+        }
+
+        SimpleAdapter simpleAdapter =  new SimpleAdapter( this, data_buku_tamu, R.layout.list_view_adapter,
+         new String[] {"transaksi_id" , "nama", "jkl", "alamat", "tanggal"},
+         new int[] {R.id.text_transaksi_id, R.id.text_nama, R.id.text_jkl, R.id.text_alamat, R.id.text_tanggal}
+        );
+        list_bukutamu.setAdapter(simpleAdapter);
+
+        list_bukutamu.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                transaksi_id = ((TextView)view.findViewById(R.id.text_transaksi_id)).getText().toString();
+                Log.e("transaksi_id", transaksi_id);
+
+                popUpAction();
+            }
+        });
+
+        hitungJumlah();
+
+        swipe_refresh.setRefreshing(false);
+
+    }
+
+    private void hitungJumlah() {
+        NumberFormat rupiahFormat = NumberFormat.getInstance(Locale.GERMANY);
+
+        SQLiteDatabase db = sqliteHelper.getReadableDatabase();
+        cursor = db.rawQuery(query_total, null);
+        cursor.moveToFirst();
+
+
+        jumlah_laki.setText(rupiahFormat.format(cursor.getDouble(1)));
+        jumlah_perempuan.setText(rupiahFormat.format(cursor.getDouble(2)));
+        jumlah_keseluruhan.setText(rupiahFormat.format(cursor.getDouble(0)));
+
+        if(!query_pencarian){
+            text_hasil_pencarian.setVisibility(View.GONE);
+        }
+        query_pencarian = false;
+    }
+
+
+    private void popUpAction(){
+        final Dialog dialog = new Dialog(MainActivity.this);
+        dialog.setContentView(R.layout.popup_action);
+        dialog.getWindow().setLayout(WindowManager.LayoutParams.MATCH_PARENT, WindowManager.LayoutParams.WRAP_CONTENT);
+
+        TextView text_hapus =   dialog.findViewById(R.id.text_hapus);
+        TextView text_edit =   dialog.findViewById(R.id.text_edit);
+
+        text_edit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                dialog.dismiss();
+                startActivity(new Intent(MainActivity.this, EditActivity.class));
+            }
+        });
+
+        text_hapus.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                TampilPesanHapusData();
+                dialog.dismiss();
+            }
+        });
+
+        dialog.show();
+    }
+
+    private void TampilPesanHapusData(){
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("Pesan Konfirmasi");
+        builder.setMessage("Anda yakin akan menghapus Data terpilih ..?");
+        builder.setPositiveButton("Ya", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.dismiss();
+                hapus_data();
+            }
+        });
+        builder.setNegativeButton("Tidak", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.dismiss();
+            }
+        });
+        builder.show();
+    }
+
+
+    private void hapus_data() {
+        SQLiteDatabase db = sqliteHelper.getWritableDatabase();
+        db.execSQL("delete from transaksi where transaksi_id='"+transaksi_id+"'");
+
+        Toast.makeText(getApplicationContext(),"Data Berhasil dihapus", Toast.LENGTH_LONG).show();
+        kasAdapter();
     }
 
     @Override
@@ -55,7 +266,7 @@ public class MainActivity extends AppCompatActivity {
 
         //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
-            return true;
+            startActivity(new Intent(MainActivity.this, SearchActivity.class));
         }
 
         return super.onOptionsItemSelected(item);
